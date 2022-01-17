a6eb814f403f37e5b8a2d41842c7caa3f9fda79d
vothuyduong <vothuyduong999@gmail.com>
2022-01-18 05:07:31 +0700

    cap nhap admin

--- models/Category.php
+++ models/Category.php
@@ -15,6 +15,16 @@
 			$this->listSP = $listSP;
 		}
 
+		public function setMaDM ($maDM)
+    	{
+        	$this->maDM = $maDM;
+    	}
+
+    	public function setTenDM ($tenDM)
+    	{
+        	$this->tenDM = $tenDM;
+    	}
+
 		static function listDanhMuc() { 
 			$db = DB::getInstance(); 
 			$sql = "SELECT * FROM DanhMuc"; 
@@ -29,6 +39,20 @@
 			return $list; 
 		}
 
+		static function getDanhMuc($maDM) { 
+			$db = DB::getInstance(); 
+			$sql = "SELECT * FROM DanhMuc WHERE MaDM = '".$maDM."'"; 
+			$req = $db->query($sql);
+			$list = [];
+
+			foreach ($req->fetchAll() as $item) { 
+				$list[] = new Category($item['MaDM'], $item['TenDM'], Type::listByDanhMuc($item['MaDM']), 
+										Product::listByDanhMuc($item['MaDM']));
+			} 
+
+			return $list[0]; 
+		}
+
 		static function getNameById($maDM) { 
 			$db = DB::getInstance(); 
 			$sql = "SELECT TenDM FROM DanhMuc WHERE MaDM = '".$maDM."'"; 
@@ -38,6 +62,50 @@
 				return $item['TenDM'];
 			} 
 
+		}
+
+		static function getByName($tenDM) { 
+			$db = DB::getInstance(); 
+			$sql = "SELECT MaDM FROM DanhMuc WHERE TenDM = '".$tenDM."'"; 
+			$req = $db->query($sql);
+
+			foreach ($req->fetchAll() as $item) { 
+				return $item['MaDM'];
+			} 
+		
+		}
+
+		static function insertDM($maDM, $tenDM) {
+			$kt_maDM = Category::getNameById($maDM);
+			if($kt_maDM != null) {
+				return 1;
+			}
+
+			$kt_tenDM = Category::getByName($tenDM);
+			if($kt_tenDM != null) {
+				return 2;
+			}
+
+			$db = DB::getInstance(); 
+			$stmt = $db->prepare('insert into DanhMuc (MaDM, TenDM) values (:MaDM, :TenDM)');
+			$stmt->bindParam(':MaDM', $maDM);
+			$stmt->bindParam(':TenDM', $tenDM);
+			$stmt->execute();
+			return 0;
+		}
+
+		static function editDM($maDM, $tenDM) {
+			$kt_tenDM = Category::getByName($tenDM);
+			if($kt_tenDM != null) {
+				return 2;
+			}
+
+			$db = DB::getInstance(); 
+			$stmt = $db->prepare('update DanhMuc set TenDM = :TenDM where MaDM = :MaDM');
+			$stmt->bindParam(':MaDM', $maDM);
+			$stmt->bindParam(':TenDM', $tenDM);
+			$stmt->execute();
+			return 0;
 		}
 	}
 ?>
